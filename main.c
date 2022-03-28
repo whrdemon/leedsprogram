67eda8a5a9062b68aad4c74cca73c6d8e0055073
whrdemon <2693901321@qq.com>
2022-03-28 22:56:36 +0800

    Finish all the functions of the library.

--- main.c
+++ main.c
@@ -7,10 +7,16 @@
 #include "user.h"
 int main()
 {
-    int a,b,c,d,p,g,s;
+    int a,b,c,d,p,g,s,bo,us,re;
 
     Book *head = (Book*)malloc(sizeof(Book));
     head->next=NULL;
+    FILE *fo= fopen("user.txt","a");
+    fprintf(fo,"librarian");
+    fprintf(fo,"\t");
+    fprintf(fo,"librarian");
+    fprintf(fo,"\n");
+    fclose(fo);
     while (1){
         a=Face();
         switch (a) {
@@ -21,18 +27,123 @@ int main()
                 break;
             case 2:
                 b = login(createList());
+                if(b==3){
+                    break;
+                }
                 while (b == 0) {
                     b = login(createList());
                 }
                 if (b == 1) {
-
-                    printf("Welcome the library!\n");
                     d=Userface();
-                    if(d==1){
-                        printf("Borrow book");
-                    }
+                    while(d){
+                        switch (d) {
 
-                }
+
+                            case 1: {
+                                FILE *fp = fopen("booklist.txt", "r");
+                                load_books(fp, head);
+                                bo = Borrowbook(head, borrow());
+                                if (bo == 0) {
+                                    printf("Borrow successfully!\n");
+                                    fclose(fp);
+                                    FILE *fm= fopen("booklist.txt","w");
+                                    store_books(fm,head);
+                                    fclose(fm);
+
+                                    d=Userface();
+                                    break;
+
+                                }
+                                else if(bo==1){
+                                    fclose(fp);
+                                    d=Userface();
+                                    break;
+                                }
+                            }
+                            case 2:{
+                                displayborrowbook(borrow());
+                                FILE *fp = fopen("booklist.txt", "r");
+                                load_books(fp, head);
+                                re=returnbook(head,borrow());
+                                if (re == 0) {
+                                    printf("Return successfully!\n");
+                                    fclose(fp);
+                                    FILE *fm= fopen("booklist.txt","w");
+                                    store_books(fm,head);
+                                    fclose(fm);
+
+                                    d=Userface();
+                                    break;
+
+                                }
+                                else if(re==1){
+                                    fclose(fp);
+                                    d=Userface();
+                                    break;
+                                }
+
+
+                            }
+                            case 3:{
+                                us=search();
+                                unsigned int year;
+                                char *title,*t,*y,*author;
+                                title=(char *)malloc(sizeof(char));
+                                author=(char *)malloc(sizeof(char));
+                                t=(char *)malloc(sizeof(char));
+                                y=(char *)malloc(sizeof(char));
+                                if(us==1){
+                                    printf("Please enter the book title:");
+                                    FILE *fp=fopen("booklist.txt","r");
+                                    load_books(fp,head);
+                                    fclose(fp);
+                                    gets(t);
+                                    gets(title);
+                                    find_book_by_title(title,head);
+                                    break;
+                                }
+                                else if(us==2){
+                                    printf("Please enter the book authors:");
+                                    FILE *fp=fopen("booklist.txt","r");
+                                    load_books(fp,head);
+                                    fclose(fp);
+                                    gets(t);
+                                    gets(author);
+                                    find_book_by_author(author,head);
+                                    break;
+
+                                }
+                                else if(us==3){
+                                    printf("Please enter the book year:");
+                                    FILE *fp=fopen("booklist.txt","r");
+                                    load_books(fp,head);
+                                    fclose(fp);
+                                    gets(t);
+                                    gets(y);
+                                    year= atoi(y);
+                                    find_book_by_year(year,head);
+                                    break;
+
+                                }
+                                else if(us==4){
+                                    d=Userface();
+                                    break;
+                                }
+
+                            }
+                            case 4:{
+                                FILE *fp=fopen("booklist.txt","r");
+                                load_books(fp,head);
+                                fclose(fp);
+                                display(head);
+                                d=Userface();
+                                break;
+                            }
+                        }
+                        if(d==5){
+                            break;
+                        }
+                }}
                 if (b == 2) {
                     printf("Welcome,librarian!\n");
                     c = Lib();
@@ -148,6 +259,7 @@ int main()
 
 
             case 3:
+                printf("Welcome to use it next time. Bye!");
                 return 0;
 
 
--- user.c
+++ user.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "user.h"
+#include "book_mangement.h"
 int Userface(){
     char a[10];
     int b;
@@ -17,7 +18,7 @@ int Userface(){
     b=strlen(a);
     while(b>=2||(a[0]!='1'&&a[0]!='2'&&a[0]!='3'&&a[0]!='4'&&a[0]!='5')){
         printf("\n");
-        printf("Sorry the option is invalid,please try again.");
+        printf("Sorry the option is invalid,please try again.\n");
         printf("Enter your choice:\n");
         printf("1.Borrow the book\n");
         printf("2.Return the book\n");
@@ -49,5 +50,194 @@ int Userface(){
 
 
 
-};
+}
+int Borrowbook(Book *head,Book *borrow){
+    FILE *fw= fopen("borrow.txt","a");
+    if(head->next==NULL){
+        printf("Sorry,there are no books in the library.");
+        return 1;
+    }
+    display(head);
+    char *i;
+    int a,b;
+    b=0;
+    i=(char *)malloc(sizeof(char));
+    printf("Please enter the ID of the book you want to borrow:");
+    scanf("%s",i);
+    if(check(i)==0){
+        printf("Please enter a positive integer.");
+        return 1;
+    }
+    a= atoi(i);
+    Book *temp=head;
+    Book *t=borrow;
+    Book *abc = borrow->next;
+    Book *no = head->next;
+    while(no)
+    {
+        if(no->id==a&&no->now==0)
+        {
+            printf("Sorry.The number of books available for borrowing is 0.");
+            return 1;
+
+        }
+        no = no->next;
+    }
+    while(abc)
+    {
+        if(abc->id==a)
+        {
+            printf("You have borrowed this book.");
+            return 1;
+
+        }
+        abc = abc->next;
+    }
+
+    while (t->next!=NULL){
+        t=t->next;
+    }
+    while(temp->next!=NULL){
+        temp=temp->next;
+        if (temp->id==a ){
+            t->id=temp->id;
+            t->year=temp->year;
+            t->authors=temp->authors;
+            t->title=temp->title;
+            t->copies=temp->copies;
+            t->now=temp->now;
+            temp->now=temp->now-1;
+            b=1;
+            fprintf(fw,"%i",temp->id);
+            fprintf(fw,";");
+            fprintf(fw,"%s",temp->title);
+            fprintf(fw,";");
+            fprintf(fw,"%s",temp->authors);
+            fprintf(fw,";");
+            fprintf(fw,"%i",temp->year);
+            fprintf(fw,"\n");
+        }
+        if(b==1){
+            fclose(fw);
+            break;
+        }
+
+
+    }
+    if(b==0){
+        printf("Invalid input, please enter a correct ID.");
+        return 1;
+    }
+    return 0;
+
+
+}
+int displayborrowbook(Book *borrow){
+    Book *temp=borrow;
+
+    while (temp->next!=NULL){
+        temp=temp->next;
+        printf("ID:");
+        printf("%i",temp->id);
+        printf("\0");
+        printf("Title:");
+        printf("%s",temp->title);
+        printf("\0");
+        printf("Authors::");
+        printf("%s",temp->authors);
+        printf("\0");
+        printf("Year:");
+        printf("%i",temp->year);
+        printf("\n");
+    }
+
+}
+Book *borrow() {
+    Book *head = (Book *) malloc(sizeof(Book));
+    head->next = NULL;
+    FILE *fp = fopen("borrow.txt", "r");
+    Book *last = head;
+    char line[300];
+    while (fgets(line, 300, fp) != NULL) {
+        Book *temp = (Book *) malloc(sizeof(Book));
+
+        temp->id = atoi(strtok(line, ";"));
+        temp->title = strddd(strtok(NULL, ";"));
+        temp->title[strlen(temp->title)] = '\0';
+
+        temp->authors = strddd(strtok(NULL, ";"));
+        temp->authors[strlen(temp->authors)] = '\0';
+        temp->year = atoi(strtok(NULL, "\n"));
+
+        last->next = temp;
+        last = temp;
+        last->next = NULL;
+
+    }
+    fclose(fp);
+    return head;
+}
+int returnbook(Book*head,Book*borrow){
+    char *i;
+    int a,b;
+    b=0;
+    if(borrow->next==NULL){
+        printf("You hava not borrowed any books.");
+        return 1;
+    }
+    i=(char *)malloc(sizeof(char));
+    printf("Please enter the book ID to return the book:");
+    scanf("%s",i);
+    if(check(i)==0){
+        printf("Please enter a positive integer.");
+        return 1;
+    }
+    a= atoi(i);
+    Book *x=head;
+    Book *temp=borrow;
+    while(x->next!=NULL){
+        x=x->next;
+        if(x->id==a){
+            x->now=x->now+1;
+            break;
+        }
+    }
+    while (temp->next!=NULL){
+        if (temp->next->id == a&&temp->next->next!=NULL){
+            temp->next= temp->next->next;;
+            b=1;
+        }
+        else if(temp->next->id == a&&temp->next->next==NULL){
+            temp->next=NULL;
+            b=1;
+        }
+
+        if(b==1){
+            break;
+        }
+        temp=temp->next;
+
+    }
+
+    if(b==0){
+        printf("Invalid input, please enter a correct ID");
+        return 1;
+    }
+    Book *t=borrow;
+    FILE *fg= fopen("borrow.txt","w");
+    while (t->next!=NULL){
+        t=t->next;
+        fprintf(fg,"%i",t->id);
+        fprintf(fg,";");
+        fprintf(fg,"%s",t->title);
+        fprintf(fg,";");
+        fprintf(fg,"%s",t->authors);
+        fprintf(fg,";");
+        fprintf(fg,"%i",t->year);
+        fprintf(fg,"\n");
+    }
+    fclose(fg);
+    return 0;
+
+}
 
